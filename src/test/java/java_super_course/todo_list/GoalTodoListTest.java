bbea76b66ab329b4bd41f1fee828e5694a68d8a2
igRock <topchiy.greg@yandex.ru>
2020-05-25 09:51:58 +0300

    active profiles

--- src/test/java/java_super_course/todo_list/GoalTodoListTest.java
+++ src/test/java/java_super_course/todo_list/GoalTodoListTest.java
@@ -18,12 +18,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.testcontainers.containers.PostgreSQLContainer;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
 @AutoConfigureTestDatabase(replace=Replace.NONE)
+@ActiveProfiles("test")
 public class GoalTodoListTest {
 
     @ClassRule
